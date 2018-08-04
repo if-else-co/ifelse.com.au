33ddf376e1b2a5c4c64e5e34465a7ebc28603fb4
Auspicus <elijahdisch@gmail.com>
2018-08-04 03:02:46 -0600

    Added help button to pricing plans

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -12,6 +12,12 @@ const Checkmark = () => (
   </svg>
 );
 
+const Question = () => (
+  <svg class="pricing__help" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM10.59 8.59a1 1 0 1 1-1.42-1.42 4 4 0 1 1 5.66 5.66l-2.12 2.12a1 1 0 1 1-1.42-1.42l2.12-2.12A2 2 0 0 0 10.6 8.6zM12 18a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+  </svg>
+);
+
 const Pricing = props => {
   const onSelect = props.onSelect || function () {};
   return (
@@ -44,7 +50,10 @@ const Pricing = props => {
         <div className="pricing__grid-item">
           <div className="pricing__plan pricing--dblue">
             <div className="pricing__overview">
-              <h3 className="pricing__plan-title">Starter</h3>
+              <a className="pricing__help-link" href="#">
+                <Question />
+              </a>
+              <h3 className="pricing__plan-title">Basic</h3>
               <div className="pricing__plan-upfront">
                 <sup>$</sup>500
               </div>
@@ -86,6 +95,9 @@ const Pricing = props => {
         <div className="pricing__grid-item">
           <div className="pricing__plan pricing--purple">
             <div className="pricing__overview">
+              <a className="pricing__help-link" href="#">
+                <Question />
+              </a>
               <h3 className="pricing__plan-title">Premium</h3>
               <div className="pricing__plan-upfront">
                 <sup>$</sup>2500
@@ -128,6 +140,9 @@ const Pricing = props => {
         <div className="pricing__grid-item">
           <div className="pricing__plan pricing--blue">
             <div className="pricing__overview">
+              <a className="pricing__help-link" href="#">
+                <Question />
+              </a>
               <h3 className="pricing__plan-title">Enterprise</h3>
               <button
                 onClick={() => onSelect(ENTERPRISE)}
