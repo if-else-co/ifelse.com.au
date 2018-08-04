344cb2690a6441df8f3a9c3fc450baf978063574
Auspicus <elijahdisch@gmail.com>
2018-08-04 02:40:44 -0600

    Added mobile styles for pricing widget

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -7,8 +7,8 @@ const PREMIUM = 'premium';
 const ENTERPRISE = 'enterprise';
 
 const Checkmark = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <path class="pricing__checkmark" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"/>
+  <svg className="pricing__checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"/>
   </svg>
 );
 
@@ -25,6 +25,7 @@ const Pricing = props => {
               <div className="pricing__plan-section-item">Domain management</div>
               <div className="pricing__plan-section-item">Hosting</div>
               <div className="pricing__plan-section-item">Custom design</div>
+              <div className="pricing__plan-section-item">Custom application</div>
             </div>
             <div className="pricing__plan-section">
               <span className="pricing__plan-section-name">Social Media</span>
@@ -41,7 +42,7 @@ const Pricing = props => {
           </div>
         </div>
         <div className="pricing__grid-item">
-          <div className="pricing__plan pricing--blue">
+          <div className="pricing__plan pricing--dblue">
             <div className="pricing__overview">
               <h3 className="pricing__plan-title">Starter</h3>
               <div className="pricing__plan-upfront">
@@ -61,17 +62,21 @@ const Pricing = props => {
             </div>
             <div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
-                <div className="pricing__plan-contents-item"></div>
-              </div>
-              <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
+                <div className="pricing__plan-contents-section-name">Website</div>
+                <div className="pricing__plan-contents-item"><span>Domain management</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Hosting</span><Checkmark /></div>
                 <div className="pricing__plan-contents-item"></div>
                 <div className="pricing__plan-contents-item"></div>
               </div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
+                <div className="pricing__plan-contents-section-name">Social media</div>
+                <div className="pricing__plan-contents-item"><span>Account management</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"></div>
+                <div className="pricing__plan-contents-item"></div>
+              </div>
+              <div className="pricing__plan-contents-section">
+                <div className="pricing__plan-contents-section-name">Support</div>
+                <div className="pricing__plan-contents-item"><span>Security updates</span><Checkmark /></div>
                 <div className="pricing__plan-contents-item"></div>
                 <div className="pricing__plan-contents-item"></div>
               </div>
@@ -79,7 +84,7 @@ const Pricing = props => {
           </div>
         </div>
         <div className="pricing__grid-item">
-          <div className="pricing__plan pricing--yellow">
+          <div className="pricing__plan pricing--purple">
             <div className="pricing__overview">
               <h3 className="pricing__plan-title">Premium</h3>
               <div className="pricing__plan-upfront">
@@ -99,25 +104,29 @@ const Pricing = props => {
             </div>
             <div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
+                <div className="pricing__plan-contents-section-name">Website</div>
+                <div className="pricing__plan-contents-item"><span>Domain management</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Hosting</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Custom design</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"></div>
               </div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
+                <div className="pricing__plan-contents-section-name">Social media</div>
+                <div className="pricing__plan-contents-item"><span>Account management</span><Checkmark /></div>
                 <div className="pricing__plan-contents-item"></div>
                 <div className="pricing__plan-contents-item"></div>
               </div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
-                <div className="pricing__plan-contents-item"><Checkmark /></div>
+                <div className="pricing__plan-contents-section-name">Support</div>
+                <div className="pricing__plan-contents-item"><span>Security updates</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Bug fixes</span><Checkmark /></div>
                 <div className="pricing__plan-contents-item"></div>
               </div>
             </div>
           </div>
         </div>
         <div className="pricing__grid-item">
-          <div className="pricing__plan pricing--purple">
+          <div className="pricing__plan pricing--blue">
             <div className="pricing__overview">
               <h3 className="pricing__plan-title">Enterprise</h3>
               <button
@@ -128,37 +137,23 @@ const Pricing = props => {
             </div>
             <div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
+                <div className="pricing__plan-contents-section-name">Website</div>
+                <div className="pricing__plan-contents-item"><span>Domain management</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Hosting</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Custom design</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Custom application</span><Checkmark /></div>
               </div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
+                <div className="pricing__plan-contents-section-name">Social media</div>
+                <div className="pricing__plan-contents-item"><span>Account management</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Content creation</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Media &amp; marketing</span><Checkmark /></div>
               </div>
               <div className="pricing__plan-contents-section">
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
-                <div className="pricing__plan-contents-item">
-                  <Checkmark />
-                </div>
+                <div className="pricing__plan-contents-section-name">Support</div>
+                <div className="pricing__plan-contents-item"><span>Security updates</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Bug fixes</span><Checkmark /></div>
+                <div className="pricing__plan-contents-item"><span>Agile development</span><Checkmark /></div>
               </div>
             </div>
           </div>
