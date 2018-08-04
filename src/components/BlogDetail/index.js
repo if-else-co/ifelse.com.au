960a2f1d6a79a95ea0781981982d8f3d3f1972b3
Auspicus <elijahdisch@gmail.com>
2018-08-04 17:41:25 -0600

    Design tweaks for blogs

--- src/components/BlogDetail/index.js
+++ src/components/BlogDetail/index.js
@@ -1,7 +1,7 @@
 import React, { createElement } from 'react';
 import marksy from 'marksy';
 
-//import { formatDate } from '../../helpers';
+import { formatDate } from '../../helpers';
 import './styles.css';
 
 const getMarkup = field => {
@@ -17,20 +17,24 @@ const BlogDetail = props => {
   let title;
   let content;
   let imgSrc;
+  let publishedDate;
+  let timeToRead;
 
   if (props.blog) {
     title = props.blog.title;
     imgSrc = props.blog.featured_image.fields.file.url;
     content = getMarkup(props.blog.body);
+    publishedDate = formatDate(new Date(props.blog.published));
+    timeToRead = Math.floor(props.blog.body.split(' ').length / 150);
   }
-
-  //let publishedDate = formatDate(new Date(props.published));
-  //let timeToRead = `${props.timeToRead} min${props.timeToRead > 1 ? 's' : ''}`;
 
   return (
     <div className="blog-detail">
       <img className="blog-detail__featured-image" src={imgSrc} alt="" />
       <h1 className="blog-detail__title">{title}</h1>
+      <div className="blog-detail__meta">
+        {publishedDate} &middot; {timeToRead} min read
+      </div>
       <div className="blog-detail__content">
         {content}
       </div>
