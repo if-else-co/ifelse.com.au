c28ab6630aa6367ae0a6c515c485709fe6e062e2
Auspicus <elijahdisch@gmail.com>
2019-03-25 10:34:01 -0600

    Avoid rendering GoogleAd in react-snap

--- src/components/GoogleAd/index.js
+++ src/components/GoogleAd/index.js
@@ -3,17 +3,22 @@ import React from 'react'
 import './styles.css'
 
 const GoogleAd = ({ adClient, adSlot, adFormat, fullWidthResponse }) => (
-  <React.Fragment>
-    <ins className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client={adClient}
-        data-ad-slot={adSlot}
-        data-ad-format={adFormat}
-        data-full-width-responsive={fullWidthResponse}></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-  </React.Fragment>
+  navigator.userAgent !== 'ReactSnap'
+  ? (
+    <React.Fragment>
+      <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client={adClient}
+          data-ad-slot={adSlot}
+          data-ad-format={adFormat}
+          data-full-width-responsive={fullWidthResponse}></ins>
+      <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    </React.Fragment>
+  ) : (
+    null
+  )
 )
 
 export default GoogleAd
